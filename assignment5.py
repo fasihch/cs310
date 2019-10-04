1d5fa593e409f7c9230b65e87d0174327013db3d
fasih ahmed <fasihch414@hotmail.com>
2019-10-04 10:10:04 -0700

    my commit

---
+++ assignment5.py
@@ -0,0 +1,17 @@
+x=6
+print(x)
+y=x+7
+print(y)
+
+print('hello world')
+print('you must be the legendary  god that comes from the sky')
+print('we have been waiting for you for a long time')
+print('our legends sys you will be very tasty with mustard')
+print('we will have a feast tonight unless you say')
+
+x=43
+x=x+1
+print(x)
+print('i hate you python')
+print('Good bye')
+
