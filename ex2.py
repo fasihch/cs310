23c7ea7342ff48e8526e043abc134b949968f154
fasih ahmed <fasihch414@hotmail.com>
2019-10-10 10:44:00 -0700

    assignment 6

--- ex2.py
+++ ex2.py
@@ -1,1 +1,62 @@
+name = input("Write your name: ")
+print("Hello", name, "!")
+
+hours = int(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+salary = hours * rate
+print("Pay: {0:.2f}".format(salary))
+
+
+width = 17
+height = 12.0
+print(width/2)
+print(type(width/2))
+print(width/2.0)
+print(type(width/2.0))
+print(height/3)
+print(type(height/3))
+print(1+2*5)
+print(type(1+2*5))
+
+from fractions import Fraction
+celsius = float(input("Enter degrees in Celsius: "))
+fahrenheit = celsius * Fraction(9, 5) + 32
+print("{0:.2f} C⁰ is {1:.2f} F⁰".format(celsius, fahrenheit))
+
+a = 35.0
+b = 12.50
+c =a*b
+print(c)
+
+hours =35.0
+rate = 12.50
+pay = hours * rate
+print(pay)
+
+name=input('What is your name?\n')
+print(name)
+
+first =10
+second =15
+print(first+second)
+
+first='100'
+second='110'
+print(first+second)
+
+first= 'fasih'
+second= 'ahmed'
+print(first+second)
+
+first= 'test'
+second= 3
+print(first*second)
+
+quotient = '7//3'
+print(quotient)
+
+remainder='7%3'
+print(remainder)
+
+
 
